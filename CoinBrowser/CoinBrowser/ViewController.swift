792ffdd09e29a416618827f4735ecc0a32ef6386
Karl Pfister <kpfister@me.com>
2021-11-23 16:49:37 -0700

    Initial Commit

---
+++ CoinBrowser/CoinBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinBrowser
+//
+//  Created by Karl Pfister on 11/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
